3b6ac7ab8d1f8f0de97b26ca9035a472960dd27d
Skorodumov Anton <scorodumov.a.v@gmail.com>
2018-10-26 14:46:45 +0700

    initial commit

---
+++ cardbag/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  cardbag
+//
+//  Created by Anton Skorodumov on 26.10.2018.
+//  Copyright © 2018 E-Lite-Telecom. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
