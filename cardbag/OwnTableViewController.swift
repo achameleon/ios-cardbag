9e9c4d12946fd3939de0e3f6b94920c1cda1199d
Anton Skorodumov <chameleon@Antons-MacBook-Pro.local>
2018-10-26 18:10:20 +0700

    added network connection

--- cardbag/OwnTableViewController.swift
+++ cardbag/OwnTableViewController.swift
@@ -9,6 +9,26 @@
 import UIKit
 import Alamofire
 
+struct TestData {
+    
+    var userId: Int = 0
+    var title: String = ""
+    var id = 0
+    var complited: Bool = false
+    
+    init (map: [String: AnyObject]) {
+        userId = map["userId"] as! Int
+        title = map["title"] as! String
+        id = map["id"] as! Int
+        complited = map["completed"] as! Bool
+    }
+    
+    func show() {
+        print ("userId = \(userId)  title = \(title)")
+    }
+    
+}
+
 class OwnTableViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
@@ -19,7 +39,27 @@ class OwnTableViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
     
-        
+        let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
+        Alamofire.request(url).responseJSON { (response) in
+            if let d = response.result.value as? [String: AnyObject] {
+                let object = TestData(map: d)
+                object.show()
+            }
+        }
+    
+        /*
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error { return }
+            guard let data = data else { return }
+            let d = try! JSONSerialization.data(withJSONObject: data, options: .prettyPrinted)
+            if let dict = d as? [String: AnyObject] {
+                let object = TestData(map: dict)
+                object.show()
+            }
+        }
+        task.resume()
+ */
     }
 
 }
