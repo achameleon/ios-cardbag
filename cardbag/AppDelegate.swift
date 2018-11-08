cb450b26fb9717b49ac73a6c32a54cbabeba9be5
Admin <admin@Mac-Admin.local>
2018-11-09 00:20:54 +0300

    Revert "Start project"
    This reverts commit 359f974fe79d4308645fb73a22ad0c9df422baa1.

--- cardbag/AppDelegate.swift
+++ cardbag/AppDelegate.swift
@@ -7,25 +7,17 @@
 //
 
 import UIKit
-import Alamofire
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     var window: UIWindow?
-    
-    
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-        ) -> Bool {
-        window = UIWindow(frame:UIScreen.main.bounds)
-        let viewController = CardsListViewController()
-        let Navigation = UINavigationController(rootViewController: viewController)
-        window?.rootViewController = Navigation
-        window?.makeKeyAndVisible()
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
         return true
     }
-    
+
 }
 
