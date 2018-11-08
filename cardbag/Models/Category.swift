8d18579c2e7a124202d2ba62e9ba4afaf4822bd6
Admin <admin@Mac-Admin.local>
2018-11-08 23:45:33 +0300

    add classes and query

---
+++ cardbag/Models/Category.swift
@@ -0,0 +1,26 @@
+//
+//  Category.swift
+//  cardbag
+//
+//  Created by Admin on 08.11.2018.
+//  Copyright © 2018 E-Lite-Telecom. All rights reserved.
+//
+
+import Foundation
+
+
+class Category
+{
+    var id: Int
+    var title: String
+    init ( a:Any )
+    {
+        guard let b = a as? [String:Any] else
+        {
+            return
+        }
+        
+        id = b["id"] as? Int ?? 0
+        title = b["title"] as? String ?? ""
+    }
+}
